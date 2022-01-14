e028809af3f3a87cd31f5f94da0414ad7badacd2
benedictfernando <fernandobenedict619@gmail.com>
2022-01-14 23:15:46 +0800

    detach CR on new line

--- oop-contactTracer/Form1.cs
+++ oop-contactTracer/Form1.cs
@@ -140,7 +140,7 @@ namespace oop_contactTracer
             if (File.Exists(database))  // If database specified above is found
             {
                 using var file = new StreamWriter(database, append: true);
-                file.WriteLine(enter);   // Append to existing .txt file
+                file.NewLine = "\n"; file.WriteLine(enter);   // Append to file
             }
             else File.WriteAllText(database, enter + "\n"); // Else, create new
         }
